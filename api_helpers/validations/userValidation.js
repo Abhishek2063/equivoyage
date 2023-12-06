86c3aa8e21e33c3a692bc461141b7d86b92d539f
abhishek <abhishekgarg2063@gmail.com>
2023-12-06 16:28:39 +0530

    user get, post, put, delete api created

--- api_helpers/validations/userValidation.js
+++ api_helpers/validations/userValidation.js
@@ -1,6 +1,6 @@
 // api_helpers/validations/userValidation.js
 
-import validator from 'validator';
+import validator from "validator";
 
 const validateUserInput = ({ firstName, lastName, email, password }) => {
   const errors = [];
@@ -24,4 +24,25 @@ const validateUserInput = ({ firstName, lastName, email, password }) => {
   return errors;
 };
 
-export { validateUserInput };
+const validateUpdateUserInput = ({ firstName, lastName, email, password }) => {
+  const errors = [];
+
+  if (firstName && !validator.isLength(firstName, { min: 1, max: 20 })) {
+    errors.push("First name must be between 1 and 20 characters.");
+  }
+
+  if (lastName && !validator.isLength(lastName, { max: 20 })) {
+    errors.push("Last name must be less than or equal to 20 characters.");
+  }
+
+  if (email && !validator.isEmail(email)) {
+    errors.push("Invalid email address.");
+  }
+
+  if (password && !validator.isLength(password, { min: 1, max: 255 })) {
+    errors.push("Password must be between 1 and 255 characters.");
+  }
+
+  return errors;
+};
+export { validateUserInput, validateUpdateUserInput };
