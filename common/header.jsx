ce2a813ad3c13d54884acc37f739cede4a6a86b1
abhishek <abhishekgarg2063@gmail.com>
2023-12-29 18:36:16 +0530

    added the logout functionality

--- common/header.jsx
+++ common/header.jsx
@@ -4,8 +4,27 @@ import Image from "next/image";
 import logoImage from "../app/assets/images/logo.png";
 import { LogoutOutlined } from "@ant-design/icons";
 import styles from "../app/assets/css/header.module.css";
+import { clearUserDetails, getUserDetails } from "@/storage/user";
+import { put } from "./api";
+import { API_LOGOUT, DASHBOARD, LOGIN } from "@/routeConstant";
+import { useRouter } from "next/navigation";
+import { message } from "antd";
+import { removeLocalData } from "@/storage/token";
 
 const Header = () => {
+  const userData = getUserDetails();
+  const router = useRouter();
+  const handleLogout = async () => {
+    const logoutUser = await put(`${API_LOGOUT}/${userData.id}`, null, true);
+    if (logoutUser.success) {
+      message.success(logoutUser.message);
+      router.push(LOGIN);
+      removeLocalData();
+      clearUserDetails();
+    } else {
+      message.error(logoutUser.message);
+    }
+  };
   return (
     <div>
       <header className={styles.header}>
@@ -17,13 +36,19 @@ const Header = () => {
               height={50}
               alt="Company Logo"
               className="mx-auto mb-2 mr-4 logoImage"
+              onClick={() =>  router.push(DASHBOARD)}
             />
             <span className={styles.appName}>Equivoyage</span>
           </div>
         </div>
         <div className={styles.headerRight}>
-          <p className={styles.welcomeText}>Welcome, Username</p>
-          <button className={styles.logoutButton}>
+          <p className={styles.welcomeText}>
+            Welcome, {userData?.firstName + " " + userData?.lastName}
+          </p>
+          <button
+            className={styles.logoutButton}
+            onClick={() => handleLogout()}
+          >
             <LogoutOutlined />
           </button>
         </div>
